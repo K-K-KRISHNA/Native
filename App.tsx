63d2c3c47884db25159a245d09d2726ee3f36ae4
K-K-KRISHNA <kanakakrishnakeerthi@gmail.com>
2025-01-06 16:35:54 +0530

    made the splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import React, { useEffect } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[]);
   return (
     <SafeAreaView>
     <View>
