de9fccd63d7430de7b19a6182543348dfa5c206a
K-K-KRISHNA <kanakakrishnakeerthi@gmail.com>
2025-01-17 21:15:52 +0530

    added push notifications for android

---
+++ src/components/Notification.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+import usePushNotification from '../utils/usePushNotification';
+
+const Notification = () => {
+  const {
+    requestUserPermission,
+    getFCMToken,
+    listenToBackgroundNotifications,
+    listenToForegroundNotifications,
+    onNotificationOpenedAppFromBackground,
+    onNotificationOpenedAppFromQuit,
+  } = usePushNotification();
+
+  useEffect(() => {
+    const listenToNotifications = () => {
+      try {
+        getFCMToken();
+        requestUserPermission();
+        onNotificationOpenedAppFromQuit();
+        listenToBackgroundNotifications();
+        listenToForegroundNotifications();
+        onNotificationOpenedAppFromBackground();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    listenToNotifications();
+  }, []);
+
+  return (
+    <View>
+      <Text>Push Notification APP</Text>
+    </View>
+  );
+};
+
+export default Notification;
